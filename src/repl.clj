f5aca6cc13965c0684fb3103a5c0b5f1c99a8bd1
abcdw <andrewtropin@gmail.com>
2018-06-06 22:09:54 +0300

    Simplify repl ns

--- src/repl.clj
+++ src/repl.clj
@@ -1,12 +1,9 @@
 (ns repl
-  (:require [clojure.tools.nrepl.server :refer [start-server]]
-            [refactor-nrepl.middleware :as refactor.nrepl]
-            [cider.nrepl :refer [cider-nrepl-handler]]))
+  (:require cider-nrepl.main))
 
 (defn -main [args]
-  (let [port (or (some-> (first *command-line-args*)
-                         (java.lang.Long/parseLong))
-                 7888)]
-    (start-server :port port
-                  :handler (refactor.nrepl/wrap-refactor cider-nrepl-handler))
-    (println "Started nREPL on port" port)))
+  (cider-nrepl.main/init
+   ["refactor-nrepl.middleware/wrap-refactor"
+    "cider.nrepl/cider-middleware"]))
+
+
