d270fc29db7f45abe7a574c0489ccd87e141a0c2
abcdw <andrewtropin@gmail.com>
2018-06-06 22:09:24 +0300

    Add dirty example of win+r for ranger hotkey

--- src/nash/config.clj
+++ src/nash/config.clj
@@ -1,24 +1,73 @@
 (ns nash.config
-  (:require [clojure.java.shell :as shell]))
+  (:require [clojure.java.shell :as shell]
+            [manifold.stream :as stream]
+            [nash.nativehook]
+            [manifold.bus :as bus]
+            [clojure.spec.alpha :as s]))
+
+;; KEYBINDINGS
+;; t for top-level, n for nested
+
+;; https://github.com/mooz/xkeysnail
 
 (def nip #(println "not implemented"))
 
+(def default-mapping
+  {"C-u" ::to-previous-hotkey-list
+   "C-f" ::toggle-transient-mode
+   "C-c" ::to-root-hotkey-list})
+
 (def default-db
-  {:nash/functions
+  {::cwd "/home/abcdw"
+
+   ::functions
    {:shell/ranger nip}
 
-   :shell/cwd "/home/abcdw"})
+   ::components
+   {::nativehook true}
+
+   ::streams
+   {:nash.keyboard/key-events (bus/event-bus)}})
 
 (def db (atom default-db))
 
 (defn defshell [k & more]
   (println more)
-  (swap! db assoc-in [:nash/functions k]
+  (swap! db assoc-in [::functions k]
          #(apply shell/sh more ;; :dir (:shell/cwd db)
                  )))
 
 (defn runfn [k]
-  ((get-in @db [:nash/functions k])))
+  ((get-in @db [::functions k])))
+
+(def nhook (nash.nativehook/init))
+(def key-event-stream (get-in nhook [:nash.plugin/streams :jnativehook/key-events]))
+
+(def key-down (atom #{}))
+(def p-stream (stream/stream))
+(stream/consume #(swap! key-down conj %) p-stream)
+(def r-stream (stream/stream))
+(stream/consume #(swap! key-down disj %) r-stream)
+
+(stream/connect
+ (bus/subscribe key-event-stream "pressed")
+ p-stream)
+
+(stream/connect
+ (bus/subscribe key-event-stream "released")
+ r-stream)
+
+(def prn-stream (stream/stream))
+
+(stream/consume (fn [x] (println key-down)) prn-stream)
+
+(stream/consume (fn [x] (if (= #{3675 24} @key-down)
+                         (future (runfn :shell/ranger)))) prn-stream)
+
+(stream/connect
+ (bus/subscribe key-event-stream "pressed")
+ prn-stream)
+
 
 (defshell :shell/ranger
   "alacritty" "-e" "ranger")
@@ -30,8 +79,8 @@
   "xrandr" "--output" "HDMI1" "--primary"
   "--mode" "1920x1200" "--right-of" "eDP1")
 
-(runfn :windowmanager/detach-hdmi)
-(runfn :windowmanager/attach-hdmi)
+;; (runfn :windowmanager/detach-hdmi)
+;; (runfn :windowmanager/attach-hdmi)
 
-(runfn :shell/ranger)
+;; (runfn :shell/ranger)
 
