5c24c4d3102256c8f8563659298f43c23936105f
Andrew Tropin <andrewtropin@gmail.com>
2019-03-22 11:34:44 +0300

    Add sample telegram bot

--- src/nash/config.clj
+++ src/nash/config.clj
@@ -4,6 +4,9 @@
   (:require [clojure.java.shell :as shell]
             [manifold.stream :as stream]
             [nash.nativehook]
+            [morse.api]
+            [morse.polling]
+            [morse.handlers]
             [manifold.bus :as bus]
             [clojure.spec.alpha :as s]))
 
@@ -207,3 +210,38 @@ It accepts a `name` as an argument and returns a pretty-printed doc.
   "--mode" "1920x1200" "--right-of" "eDP1")
 
 ;; TODO: add cwd to i3status
+
+
+(defn notify [message]
+  (shell/sh "notify-send" "-t" "3000" "-u" "normal" message))
+
+;; (notify "hi there")
+
+(def token "")
+
+                                        ; This will define bot-api function, which later could be
+                                        ; used to start your bot
+(morse.handlers/defhandler bot-api
+                                        ; Each bot has to handle /start and /help commands.
+                                        ; This could be done in form of a function:
+  (morse.handlers/command-fn "start" (fn [{{id :id :as chat} :chat}]
+                          (println "Bot joined new chat: " chat)
+                          (morse.api/send-text token id "Welcome!")))
+
+                                        ; You can use short syntax for same purposes
+                                        ; Destructuring works same way as in function above
+  (morse.handlers/command "help" {{id :id :as chat} :chat}
+             (println "Help was requested in " chat)
+             (morse.api/send-text token id "Help is on the way"))
+
+                                        ; Handlers will be applied until there are any of those
+                                        ; returns non-nil result processing update.
+
+                                        ; Note that sending stuff to the user returns non-nil
+                                        ; response from Telegram API.
+
+                                        ; So match-all catch-through case would look something like this:
+  (morse.handlers/message message (println "Intercepted message:" message)))
+
+
+(def channel (morse.polling/start token bot-api {:timeout 30}))
